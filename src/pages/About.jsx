31c5b599c9582afed44ed0296fa7a1ae6cd3ffd9
ankittsharmaa <ankitbalotra1@gmail.com>
2024-08-31 01:21:55 +0530

    EXPANDED JSON

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,11 +7,10 @@ function About() {
       <div className="section">
         <div className="title">About Me</div>
         <p>
-          Hello. I am doing my Bachelors in Infomation Technology from VJTI
-          Mumbai,one of the oldest engineerng college in India. I am always up
-          for exploring new arenas for learning. I have a special non-academic
-          interest in history,knowing about human behaviour and psychology. I ❤️
-          eating food and trying different cuisines.
+          Hello. I am currently working as Software Analyst at Citi in Pune. I have done my Bachelors in Infomation Technology from VJTI
+          Mumbai. I am always up for exploring new arenas for learning. I have a special non-academic
+          interest in philosophy and psychology. I ❤️
+          travelling and trying different cuisines.
         </p>
       </div>
       
@@ -22,19 +21,8 @@ function About() {
             I ❤️ computer programming and loves problem solving using computational approach. I have an keen intersest
             in exploring different computer programming concepts.
           </p><br />
-          <h1>Wev Developer</h1><br />
-          <p>I am beginner level web developer and currently learning MERN stack.I have good knowledge of
-            web designing and styling.
-          </p><br />
-          <h1>Content Creator</h1><br />
-          <p>
-            I do create content for my Youtube channel named 
-            <a href="http://www.youtube.com/ninjaarashii">
-              <h1>Helpidea by Ankit</h1>
-            </a>
-            where my main motive is to help student community.There I am having
-            subscriber base over 7.5K.
-          </p>
+
+    
       </div>
      
     </div>
